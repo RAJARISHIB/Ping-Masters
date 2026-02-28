27fba6d5713766384c591012e67a2b1a522b75c6
RAJARISHIB <rajarishi448@gmail.com>
2026-02-28 12:16:32 +0530

    clean env

--- ping_masters_ui/src/environments/environment.ts
+++ ping_masters_ui/src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDhdA43jDaEWc_wic8-yL1nI-0Q9iQGSHc",
-    authDomain: "pingmasters-e90a2.firebaseapp.com",
-    projectId: "pingmasters-e90a2",
-    storageBucket: "pingmasters-e90a2.firebasestorage.app",
-    messagingSenderId: "517992040567",
-    appId: "1:517992040567:web:538e24c3a5646efced9e72",
-    measurementId: "G-2B8EY7MZD8"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   },
-  razorpayKey: "rzp_test_REJ6UbWPqZ8yMn",
-  RAZORPAY_KEY_SECRET: "hS7wbtgUHpOV8eXJLoaWRN5",
-  apiUrl: "http://127.0.0.1:8000",
+  razorpayKey: "xxxxxxxxxxxxxxxxx",
+  RAZORPAY_KEY_SECRET: "xxxxxxxxxxxxxxxxxxx",
+  apiUrl: "xxxxxxxxxxxxxxx",
 };
